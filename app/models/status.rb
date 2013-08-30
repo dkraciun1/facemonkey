cca751511d6d52859a6a1769fc6bd4cfeffe4cf8
Daniel Kraciun <kraciun@gmail.com>
2013-08-29 21:22:15 -0400

    Created a rails app and added status updates

---
+++ app/models/status.rb
@@ -0,0 +1,3 @@
+class Status < ActiveRecord::Base
+  attr_accessible :Content, :name, :text
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facemonkey::Application.config.secret_token = 'af9c9ad4efe64f7eac001e6f18c52c74d12e4af8e379afc563ce9b34cf0b8fa9d847ca44a3fc030cee7c93cf4a616289cfed310c887f3c333413e15561c4eccb'
